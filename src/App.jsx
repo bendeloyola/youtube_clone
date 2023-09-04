77ed84231717028f7eee9ae71168f7bf0f948ff5
bendeloyola <bendeloyola2017@gmail.com>
2023-09-05 03:06:46 +0800

    finished ChannelCard and start working on ChannelDetail

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Box } from '@mui/material'
-import { Navbar, Feed, ChannelDetail, VideoDetail, SearchFeed } from './components'
+import { Navbar, Feed, VideoDetail, SearchFeed } from './components'
+import ChannelDetail from "./components/ChannelDetail"
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Feed />} />
           <Route path="/video/:id" element={<VideoDetail />} />
-          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail/>} />
           <Route path="/search/:searchTerm" element={<SearchFeed />} />
         </Routes>
       </Box>
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { Videos, ChannelCard } from './'
+import { fetchFromApi } from '../utils/fetchFromApi'
 
 const ChannelDetail = () => {
+
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]))
+
+    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items))
+
+  }, [id])
+
+
   return (
-    <div>ChannelDetail</div>
+    <Box>{id}</Box>
   )
 }
 
